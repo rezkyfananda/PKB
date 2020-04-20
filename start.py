7cc2bc1b4504eb18b97357077145517573b55d16
rezkyfananda <37279620+rezkyfananda@users.noreply.github.com>
2020-04-20 09:06:34 +0700

    Update start.py

--- start.py
+++ start.py
@@ -1,8 +1,8 @@
-from mnist import MNIST
 import numpy as np
 from lib import *
+from mnist.loader import MNIST
 
-mndata = MNIST('.data')
+mndata = MNIST('./data')
 images, labels = mndata.load_training()
 t_images, t_labels = mndata.load_testing()
 
@@ -12,3 +12,10 @@ labels = np.array(labels)
 t_labels = np.array(t_labels)
 
 print_statistics(images, t_images, labels, t_labels)
+
+(m,n) = images.shape
+bias = np.ones((m,1))
+images = np.hstack((bias,images))
+(m2,n2) = t_images.shape
+t_bias = np.ones((m2,1))
+t_images = np.hstack((t_bias,t_images))
